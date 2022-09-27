da8814bf6692a05b1be587e741b98f2d2bd84593
giuliana bacchini <90486190+milagrosN13@users.noreply.github.com>
2022-09-26 21:55:42 -0300

    Delete .env.php

--- .env.php
+++
@@ -1,9 +0,0 @@
-<?php
-function credenciales() {
-    return [
-        'usuario'=>'juan' ,
-        'clave'=>'supersecreto',
-        'servidor'=>'localhost',
-        'base_de_datos'=>'banco'
-    ];
-}
